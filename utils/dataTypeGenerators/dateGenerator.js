44f0c0c294187d95cb5d5191dccbe8d2205d2d2f
sstepowany <stepowany.sebastian@gmail.com>
2018-12-07 20:51:21 +0100

    Changing strategies to start generation from initial data.

--- utils/dataTypeGenerators/dateGenerator.js
+++ utils/dataTypeGenerators/dateGenerator.js
@@ -7,14 +7,14 @@ class DateGenerator extends BaseGenerator {
         super();
         this.allowedStrategiesMap = _.assign(this.allowedStrategiesMap, {
             add: function* (startDate, metaInfo) {
-                let date = moment(startDate).format(metaInfo.dateFormat);
+                let date = moment(startDate).subtract(metaInfo.interval, metaInfo.intervalUnit).format(metaInfo.dateFormat);
                 while (true) {
                     date = moment(date).add(metaInfo.interval, metaInfo.intervalUnit).format(metaInfo.dateFormat);
                     yield date;
                 }
             },
             remove: function* (startDate, metaInfo) {
-                let date = moment(startDate).format(metaInfo.dateFormat);
+                let date = moment(startDate).add(metaInfo.interval, metaInfo.intervalUnit).format(metaInfo.dateFormat);
                 while (true) {
                     date = moment(date).subtract(metaInfo.interval, metaInfo.intervalUnit).format(metaInfo.dateFormat);
                     yield date;
