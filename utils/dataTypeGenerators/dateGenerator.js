ebe7437356f482cedbc6ae8d283e55e7419f1b38
sstepowany <sstepowany@future-processing.com>
2019-05-15 11:42:28 +0200

    Adding option to change output format for date type generator.

--- utils/dataTypeGenerators/dateGenerator.js
+++ utils/dataTypeGenerators/dateGenerator.js
@@ -10,14 +10,16 @@ class DateGenerator extends BaseGenerator {
                 let date = moment.utc(startDate).subtract(metaInfo.interval, metaInfo.intervalUnit).format(metaInfo.dateFormat);
                 while (true) {
                     date = moment.utc(date).add(metaInfo.interval, metaInfo.intervalUnit).format(metaInfo.dateFormat);
-                    yield date;
+                    const outputDate = metaInfo.dateOutputFormat ? moment.utc(date).format(metaInfo.dateOutputFormat) : date;
+                    yield outputDate;
                 }
             },
             remove: function* (startDate, metaInfo) {
                 let date = moment.utc(startDate).add(metaInfo.interval, metaInfo.intervalUnit).format(metaInfo.dateFormat);
                 while (true) {
                     date = moment.utc(date).subtract(metaInfo.interval, metaInfo.intervalUnit).format(metaInfo.dateFormat);
-                    yield date;
+                    const outputDate = metaInfo.dateOutputFormat ? moment.utc(date).format(metaInfo.dateOutputFormat) : date;
+                    yield outputDate;
                 }
             }
         });
